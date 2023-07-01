99f2f3f0b150ed5a3c4933322e788e3344d61ee2
Aneesh <aneesh1701@gmail.com>
2023-07-01 20:23:42 +0530

    feat(rs-os): add test handler

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
 
@@ -7,13 +9,18 @@ mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello println! Here is a number {}", "45");
-    panic!("PANIC!!!");
-
+    println!("Hello world!");
     loop {}
 }
 
-/* Function called on panic */
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
