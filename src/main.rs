e7e23bc6c0b7798c20dc42d83f6f14d13f0573ba
Aneesh <aneesh1701@gmail.com>
2023-07-01 23:10:03 +0530

    feat(tests): add trivial_assertion test

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -9,7 +10,11 @@ mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello world!");
+    println!("Hello World!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -19,6 +24,13 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion...");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
 
 #[panic_handler]
