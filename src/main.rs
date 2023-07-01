6821b1b64ba7481c76e6343809948e706afe4f5e
Aneesh <aneesh1701@gmail.com>
2023-07-01 20:15:43 +0530

    feat(rs-os): implement panic! macro

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,14 @@ mod vga_buffer;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello println! Here is a number {}", "45");
+    panic!("PANIC!!!");
 
     loop {}
 }
 
 /* Function called on panic */
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
