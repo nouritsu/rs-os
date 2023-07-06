a70fccb036ae22740f3a85c1500d5f251ec3640e
Aneesh <aneesh1701@gmail.com>
2023-07-06 17:27:54 +0530

    feat(interrupts): add interrupt descriptor table

---
+++ src/interrupts.rs
@@ -0,0 +1,21 @@
+use crate::println;
+use lazy_static::lazy_static;
+use x86_64::structures::idt::{
+    InterruptDescriptorTable as IDescriptorTable, InterruptStackFrame as IStackFrame,
+};
+
+lazy_static! {
+    static ref IDT: IDescriptorTable = {
+        let mut idt = IDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
+
+pub fn init_idt() {
+    IDT.load();
+}
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: IStackFrame) {
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
+pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
 
